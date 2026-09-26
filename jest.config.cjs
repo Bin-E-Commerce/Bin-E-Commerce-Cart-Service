ff2905d6108d3795d6f11d86ea39749712c922da
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2026-09-26 14:19:53 +0700

    build(config): configure cart path aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,14 @@
 module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: ".",
-  testRegex: ".*\\.spec\\.ts$",
-  transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
-  },
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageDirectory: "./coverage",
-  testEnvironment: "node",
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
 };
